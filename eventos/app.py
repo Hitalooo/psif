3f933435cf3978c74cac8aeb620b2ecf7dc66bc3
Hitalooo <hitalo.azevedo@escolar.ifrn.edu.br>
2024-08-20 10:06:05 -0300

    .

--- eventos/app.py
+++ eventos/app.py
@@ -1,40 +1,34 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/submit', methods=['GET'])
 def submit():
-        opcao = request.args.get('opcao')
-        return f'A opção escolhida foi : {opcao}'
-
-
-#     if'color' in request.cookies:
-#          return render_template('index.html', cor=request.cookies['color'])
-#     return render_template('index.html', cor='white')
-
-# @app.route('/evento')
-# def evento():
-#     evento = request.args.get('evento')
+    opcao = request.args.get('opcao')
     
-#     if 'evento' in request.cookies:
-#         if evento != request.cookies['evento']:
+    # Redireciona para a página correspondente à opção selecionada
+    if opcao == 'Piscina':
+        return redirect(url_for('piscina'))
+    elif opcao == 'Churras':
+        return redirect(url_for('churras'))
+    elif opcao == 'Quer dar a bunda mesmo?':
+        return redirect(url_for('darabunda'))
+    else:
+        return 'Opção inválida'
 
-#             template = render_template('evento.html')
-#             response = make_response(template)
-#             response.delete_cookie('evento')
-#             response.set_cookie('evento',value= string )
-#             return response
-        
-#         else:
-#              return render_template('evento.html', string=request.cookies['evento'])
-        
-#     else:
-#             template = render_template('evento.html')
-#             response = make_response(template)
-#             response.delete_cookie('evento')
-#             response.set_cookie('evento', string)
-#             return response
+@app.route('/piscina')
+def piscina():
+    return render_template('piscina.html')
+
+@app.route('/churras')
+def churras():
+    return render_template('churras.html')
+
+@app.route('/dara')
+def darabunda():
+    return render_template('darabunda.html')
+
