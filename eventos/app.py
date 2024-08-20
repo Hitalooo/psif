631881af32193bd4496604cd4f54a4d9041c7092
Hitalooo <hitalo.azevedo@escolar.ifrn.edu.br>
2024-08-20 09:45:59 -0300

    .

---
+++ eventos/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return render_template('index.html')
+
+@app.route('/submit', methods=['GET'])
+def submit():
+        opcao = request.args.get('opcao')
+        return f'A opção escolhida foi : {opcao}'
+
+
+#     if'color' in request.cookies:
+#          return render_template('index.html', cor=request.cookies['color'])
+#     return render_template('index.html', cor='white')
+
+# @app.route('/evento')
+# def evento():
+#     evento = request.args.get('evento')
+    
+#     if 'evento' in request.cookies:
+#         if evento != request.cookies['evento']:
+
+#             template = render_template('evento.html')
+#             response = make_response(template)
+#             response.delete_cookie('evento')
+#             response.set_cookie('evento',value= string )
+#             return response
+        
+#         else:
+#              return render_template('evento.html', string=request.cookies['evento'])
+        
+#     else:
+#             template = render_template('evento.html')
+#             response = make_response(template)
+#             response.delete_cookie('evento')
+#             response.set_cookie('evento', string)
+#             return response
