25f339d36bced12509c2978bcd31cebed4cc1b62
Diogo Amador <dtuberscaicogo@gmail.com>
2024-09-10 10:16:28 -0300

    .

--- app.py
+++ app.py
@@ -42,26 +42,23 @@ def planilha():
 
 @app.route('/evento')
 def evento():
-    return render_template('evento.html')
+    return render_template('escolha_evento.html')
 
 @app.route('/submit', methods=['GET'])
 def submit():
     opcao = request.args.get('opcao')
     
     if opcao == 'Piscina':
-        return redirect(url_for('piscina'))
-    elif opcao == 'Churras':
-        return redirect(url_for('churras'))
+        return redirect(url_for('evento_detalhe', nome_evento='Piscina'))
+    elif opcao == 'Churrasco':
+        return redirect(url_for('evento_detalhe', nome_evento='Churrasco'))
     else:
         return 'Opção inválida'
 
-@app.route('/piscina')
-def piscina():
-    return render_template('piscina.html')
+@app.route('/evento/<nome_evento>')
+def evento_detalhe(nome_evento):
+    return render_template('evento_detalhe.html', nome_evento=nome_evento)
 
-@app.route('/churrasco')
-def churras():
-    return render_template('churrasco.html')
 
 ####################################################################
 
@@ -73,9 +70,7 @@ def juros():
             taxa_juros = float(request.form['taxa'])
             periodo_meses = float(request.form['periodo'])
             num_pessoas = request.form.get('pessoas', 1)
-            # valor_mensal = float(request.form.get('valor_mensal', 0))
-            
-            # Verifique se o campo 'pessoas' não está vazio e é um número inteiro válido
+
             num_pessoas = int(num_pessoas) if num_pessoas else 1  # Definindo um valor padrão para evitar divisão por zero
             
             # Cálculo de juros simples
@@ -88,30 +83,6 @@ def juros():
             # Dividindo a mensalidade pelo número de pessoas
             mensalidade_por_pessoa_simples = mensalidade_simples / num_pessoas if num_pessoas > 0 else None
 
-            # COLOCAR JUROS COMPOSTOS DEPOIS
-
-            # # Cálculo de juros compostos
-            # taxa_juros_decimal = taxa_juros / 100
-            # numero_compostos = 12  # Supondo que os juros são compostos mensalmente
-            # montante_composto = objetivo * (1 + taxa_juros_decimal / numero_compostos) ** (numero_compostos * periodo_anos)
-            # juros_compostos = montante_composto - objetivo
-            
-            # # Cálculo da mensalidade para juros compostos
-            # mensalidade_composta = montante_composto / periodo_meses
-            
-            # # Dividindo a mensalidade pelo número de pessoas
-            # mensalidade_por_pessoa_composta = mensalidade_composta / num_pessoas if num_pessoas > 0 else None
-
-            # # Cálculo do montante total baseado no valor mensal para juros compostos
-            # if valor_mensal > 0:
-            #     montante_total_mensal = valor_mensal * periodo_meses
-            #     valor_total_composto_mensal = montante_total_mensal * (1 + taxa_juros_decimal / numero_compostos) ** (numero_compostos * periodo_anos)
-            #     juros_compostos_mensal = valor_total_composto_mensal - montante_total_mensal
-            # else:
-            #     valor_total_composto_mensal = None
-            #     juros_compostos_mensal = None
-
-# Arredondando os valores para duas casas decimais
             objetivo = round(objetivo, 2)
             taxa_juros = round(taxa_juros, 2)
             periodo_meses = round(periodo_meses, 2)
@@ -119,30 +90,15 @@ def juros():
             mensalidade_simples = round(mensalidade_simples, 2)
             mensalidade_por_pessoa_simples = round(mensalidade_por_pessoa_simples, 2) if mensalidade_por_pessoa_simples is not None else None
             
-            # montante_composto = round(montante_composto, 2)
-            # juros_compostos = round(juros_compostos, 2)
-            # mensalidade_composta = round(mensalidade_composta, 2)
-            # mensalidade_por_pessoa_composta = round(mensalidade_por_pessoa_composta, 2) if mensalidade_por_pessoa_composta is not None else None
-            
-            # if valor_total_composto_mensal is not None:
-            #     valor_total_composto_mensal = round(valor_total_composto_mensal, 2)
-            #     juros_compostos_mensal = round(juros_compostos_mensal, 2)
 
             return render_template('juros.html', 
                                    objetivo=objetivo, 
                                    taxa_juros=taxa_juros, 
                                    valor_total_simples=valor_total_simples,
-                                #    montante_composto=montante_composto,
                                    periodo_meses=periodo_meses,
                                    mensalidade_simples=mensalidade_simples,
-                                #    mensalidade_composta=mensalidade_composta,
                                    num_pessoas=num_pessoas,
                                    mensalidade_por_pessoa_simples=mensalidade_por_pessoa_simples,
-                                #    mensalidade_por_pessoa_composta=mensalidade_por_pessoa_composta,
-                                #    juros_compostos=juros_compostos,
-                                #    valor_mensal=valor_mensal,
-                                #    valor_total_composto_mensal=valor_total_composto_mensal,
-                                #    juros_compostos_mensal=juros_compostos_mensal
                                 )
         except ValueError:
             return render_template('juros.html', error="Por favor, insira valores numéricos válidos.")
