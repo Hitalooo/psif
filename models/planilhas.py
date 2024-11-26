7f4a5114b7b193498833feb6b04334fe4195b680
Ciro <ciromoraismedeiros@gmail.com>
2024-11-26 08:35:48 -0300

    Fazer Planilha herdar de models.base.Base

--- models/planilhas.py
+++ models/planilhas.py
@@ -1,47 +1,15 @@
-from database.db import obter_conexao
+from models.base import Base
 
 from models.lancamentos import Lancamento
 
-class Planilha:
+class Planilha(Base):
     def __init__(self, descricao, objetivo, data_ini, data_fim, lancamentos=[]):
+        super().__init__(tabela='planilhas')
         self.descricao = descricao
         self.objetivo = objetivo
         self.data_ini = data_ini
         self.data_fim = data_fim
         self.lancamentos = lancamentos
-
-    def salvar(self):
-        '''Salva no banco.'''
-        conn = obter_conexao()
-        conn.execute('INSERT INTO planilhas (descricao, objetivo, data_ini, data_fim) VALUES (?, ?, ?, ?)',
-                     (self.descricao, self.objetivo, self.data_ini, self.data_fim))
-        conn.commit()
-        # TODO: Se o id acabou de ser criado, atribuir a self
-        conn.close()
-    
-    @classmethod
-    def consultar(cls, sqlquery: str, parametros: tuple = tuple()) -> list['Planilha']:
-        '''Executa uma consulta SQL e retorna as planilhas.
-        
-        Parâmetros:
-            - sqlquery: A consulta SQL a executar.
-            - parametros: Os parâmetros para o método execute da conexão com o banco de dados.
-        
-        Retorna:
-            A lista de Planilhas encontradas.'''
-        conn = obter_conexao()
-        dados = conn.execute(sqlquery, parametros).fetchall()
-        planilhas = []
-        for linha in dados:
-            id = linha[0]
-            descricao = linha[1]
-            objetivo = linha[2]
-            data_ini = linha[3]
-            data_fim = linha[4]
-            p = cls(descricao, objetivo, data_ini, data_fim)
-            p.id = id
-            planilhas += [p]
-        return planilhas
 
     @classmethod
     def find(cls, id: int, carregar_lancamentos: bool = True) -> 'Planilha | None':
@@ -54,4 +22,30 @@ class Planilha:
         p = res[0]
         if carregar_lancamentos:
             p.lancamentos = Lancamento.consultar('SELECT * FROM lancamentos WHERE id_planilha = ?', (p.id,))
+        return p
+
+    def _atributos(self) -> dict:
+        '''Retorna o dicionário dos atributos na mesma ordem da tabela `planilha`.
+        É usado na classe models.base.Modelo para salvar as entidades no banco.
+        '''
+        p = {
+            'descricao': self.descricao,
+            'objetivo': self.objetivo,
+            'data_ini': self.data_ini,
+            'data_fim': self.data_fim,
+        }
+        return p
+
+    @classmethod
+    def _carregar_registro(cls, registro: list) -> 'Planilha':
+        '''Carrega um objeto a partir do `registro` que veio de uma consulta no banco.
+        É usado na classe models.base.Modelo para carregar as entidades nas consultas.
+        '''
+        id = registro[0]
+        descricao = registro[1]
+        objetivo = registro[2]
+        data_ini = registro[3]
+        data_fim = registro[4]
+        p = cls(descricao, objetivo, data_ini, data_fim)
+        p.id = id
         return p
